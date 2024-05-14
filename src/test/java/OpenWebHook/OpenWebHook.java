cf2223e8fff738d515c6a5c8fca8234c5a3e648a
blackcoyote <sander.goor@telenet.be>
2024-05-14 20:02:18 +0200

    rename some things

--- src/test/java/OpenWebHook/OpenWebHook.java
+++ src/test/java/OpenWebHook/OpenWebHook.java
@@ -1,13 +1,13 @@
-package com.example;
+package OpenWebHook;
 
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
 
-public class ExamplePluginTest
+public class OpenWebHook
 {
 	public static void main(String[] args) throws Exception
 	{
-		ExternalPluginManager.loadBuiltin(ExamplePlugin.class);
+		ExternalPluginManager.loadBuiltin(OpenWebHookPlugin.class);
 		RuneLite.main(args);
 	}
 }
